d10db1e33f300f212dfddc469a7cf5b2d56d903b
Andrew <dr3w.an@yandex.ru>
2019-04-11 23:35:16 +0300

    Fix practical work #2

--- practice/2.cc
+++ practice/2.cc
@@ -3,19 +3,25 @@
 
 using namespace std;
 
-void task_1(double a, double b, double *n, double *m) {
-    *n = a >= b ? cbrt(a - b) : a * a + (a - b) / sin(a * b);
+void task_1(double a, double b) {
+    double n, m;
 
-    if (*n < b)
-        *m = (*n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(*n));
-    else if (*n == b)
-        *m = b * b + tan(*n * a);
+    n = a >= b ? cbrt(a - b) : a * a + (a - b) / sin(a * b);
+
+    if (n < b)
+        m = (n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(n));
+    else if (n == b)
+        m = b * b + tan(n * a);
     else
-        *m = pow(b, 3) + *n * a * a;
+        m = pow(b, 3) + n * a * a;
+
+    cout << "a = " << a << "; b = " << b << "; n = " << n << "; m = " << m << endl;
 }
 
 
-void task_2(double a, double b, double *z, double *t) {
+void task_2(double a, double b) {
+    double z, t;
+
     z = a < b ? sqrt(abs(a * a - b * b)) : 1 - 2 * cos(a) * sin(b);
 
     if (z < b)
